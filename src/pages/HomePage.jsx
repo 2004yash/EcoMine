4abeeda0d0676c0ebae0f304758b921dbb323feb
2004yash <134356616+2004yash@users.noreply.github.com>
2024-08-26 20:15:53 +0530

    chenges in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,12 +4,14 @@ import Hero from '../components/hero/hero';
 import Button from '../components/button/button'; 
 import Hero3 from '../components/hero2/hero3';
 import SignUpPage from '../components/sign/sign'; // Updated import
+import Menu from '../components/menu/menu'
 
 const HomePage = () => {
   return (
     <div>
       <Navbar /> {/* Render the Navbar component with JSX */}
       <Hero /> {/* Render the Hero component with JSX */}
+      <Menu />
       <Button />
       <Hero3/>
       <SignUpPage /> {/* Render the SignUpPage component with JSX */}
