e8a0ea7bb95cca3e4e9a754667b57ac24c07e453
2004yash <134356616+2004yash@users.noreply.github.com>
2024-09-03 18:36:26 +0530

    carbon credit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CarbonForm from './pages/CarbonForm'; // Correct path for CarbonForm component
+import Marketplace from './components/carbon credit/Marketplace';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,6 +14,7 @@ root.render(
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/carbonform/*" element={<CarbonForm />} />
+        <Route path="/carbon" element={<Marketplace />} />
 
 
         {/* Add more routes as needed */}
