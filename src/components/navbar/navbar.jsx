dcd5c1072ee7dc1d03d8e492a436300898ad494f
Ayush Singh <ayush1009208@gmail.com>
2024-09-09 01:57:08 +0530

    Did some major changes plzz pull

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './navbar.css'; 
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-     
+        {/* Logo */}
         <div className="logo">
           <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt="Logo" className="logo-img" />
         </div>
 
-      
+        {/* Right-side menu */}
         <div className="menu-right">
-          <a href="#home" className="nav-link">Home</a>
-          <a href="#about" className="nav-link">About</a>
-          <a href="#services" className="nav-link">Services</a>
-          <a href="#contact" className="nav-link">Contact</a>
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/carbonform" className="nav-link">Carbon Form</Link>
+          <Link to="/carbon" className="nav-link">Marketplace</Link>
+          <Link to="/result" className="nav-link">Result</Link>
+          <Link to="/dashboard" className="nav-link">Profile</Link> {/* Profile now links to Dashboard */}
         </div>
       </div>
     </nav>
